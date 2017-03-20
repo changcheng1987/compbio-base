d5cef2c3e1fb9d9309eb5d742fd120f6f8b1ea23
JurgenCox <cox@biochem.mpg.de>
2017-03-20 16:28:43 +0100

    Preparation for scalable table

--- BaseLib/Forms/Table/TableView.cs
+++ BaseLib/Forms/Table/TableView.cs
@@ -20,9 +20,24 @@ namespace BaseLib.Forms.Table{
 		private double[] selectionAgentColVals;
 		private readonly TextBox auxTextBox;
 		private SplitContainer splitContainer;
+		private TableLayoutPanel tableLayoutPanel1;
+		private TableLayoutPanel tableLayoutPanel2;
+		private Button selectionAgentButton;
+		private Button textButton;
+		private Label itemsLabel;
+		private Label selectedLabel;
+		private Panel mainPanel;
+		private ComboBox scaleFactorComboBox;
+		private float userSf = 1;
+		public float SfX { get; }
+		public float SfY { get; }
 
 		public TableView(){
 			InitializeComponent();
+			SfX = WpfUtils.GetDpiScaleX();
+			SfY = WpfUtils.GetDpiScaleX();
+			InitializeComponent2();
+			scaleFactorComboBox.SelectedIndex = 3;
 			tableView = new CompoundScrollableControl{Dock = DockStyle.Fill, Margin = new Padding(0)};
 			tableViewWf = new TableViewControlModel();
 			tableView.Client = tableViewWf;
@@ -38,6 +53,159 @@ namespace BaseLib.Forms.Table{
 			selectionAgentButton.Click += SelectionAgentButton_OnClick;
 			KeyDown += (sender, args) => tableView.Focus();
 			auxTextBox = new TextBox{Dock = DockStyle.Fill, Padding = new Padding(0), Multiline = true, ReadOnly = true};
+			scaleFactorComboBox.SelectedIndexChanged += (sender, args) =>{
+				switch (scaleFactorComboBox.SelectedIndex){
+					case 0:
+						userSf = 0.2f;
+						break;
+					case 1:
+						userSf = 0.5f;
+						break;
+					case 2:
+						userSf = 0.7f;
+						break;
+					case 3:
+						userSf = 1f;
+						break;
+					case 4:
+						userSf = 1.5f;
+						break;
+					case 5:
+						userSf = 2f;
+						break;
+					case 6:
+						userSf = 4f;
+						break;
+				}
+			};
+		}
+
+		private void InitializeComponent2(){
+			tableLayoutPanel1 = new TableLayoutPanel();
+			tableLayoutPanel2 = new TableLayoutPanel();
+			selectionAgentButton = new Button();
+			textButton = new Button();
+			itemsLabel = new Label();
+			selectedLabel = new Label();
+			mainPanel = new Panel();
+			scaleFactorComboBox = new ComboBox();
+			tableLayoutPanel1.SuspendLayout();
+			tableLayoutPanel2.SuspendLayout();
+			SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			tableLayoutPanel1.ColumnCount = 1;
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+			tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 1);
+			tableLayoutPanel1.Controls.Add(mainPanel, 0, 0);
+			tableLayoutPanel1.Dock = DockStyle.Fill;
+			tableLayoutPanel1.Location = new Point(0, 0);
+			tableLayoutPanel1.Margin = new Padding(0);
+			tableLayoutPanel1.Name = "tableLayoutPanel1";
+			tableLayoutPanel1.RowCount = 2;
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F*SfY));
+			tableLayoutPanel1.Size = new Size(523, 538);
+			tableLayoutPanel1.TabIndex = 0;
+			// 
+			// tableLayoutPanel2
+			// 
+			tableLayoutPanel2.ColumnCount = 6;
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle());
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle());
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F*SfX));
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F*SfX));
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F*SfX));
+			tableLayoutPanel2.Controls.Add(selectionAgentButton, 3, 0);
+			tableLayoutPanel2.Controls.Add(textButton, 5, 0);
+			tableLayoutPanel2.Controls.Add(itemsLabel, 0, 0);
+			tableLayoutPanel2.Controls.Add(selectedLabel, 1, 0);
+			tableLayoutPanel2.Controls.Add(scaleFactorComboBox, 4, 0);
+			tableLayoutPanel2.Dock = DockStyle.Fill;
+			tableLayoutPanel2.Location = new Point(0, 518);
+			tableLayoutPanel2.Margin = new Padding(0);
+			tableLayoutPanel2.Name = "tableLayoutPanel2";
+			tableLayoutPanel2.RowCount = 1;
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+			tableLayoutPanel2.Size = new Size(523, 20);
+			tableLayoutPanel2.TabIndex = 0;
+			// 
+			// selectionAgentButton
+			// 
+			selectionAgentButton.Dock = DockStyle.Fill;
+			selectionAgentButton.Location = new Point(433, 0);
+			selectionAgentButton.Margin = new Padding(0);
+			selectionAgentButton.Name = "selectionAgentButton";
+			selectionAgentButton.Size = new Size(20, 20);
+			selectionAgentButton.TabIndex = 0;
+			selectionAgentButton.UseVisualStyleBackColor = true;
+			selectionAgentButton.Visible = false;
+			// 
+			// textButton
+			// 
+			textButton.Dock = DockStyle.Fill;
+			textButton.Location = new Point(503, 0);
+			textButton.Margin = new Padding(0);
+			textButton.Name = "textButton";
+			textButton.Size = new Size(20, 20);
+			textButton.TabIndex = 1;
+			textButton.UseVisualStyleBackColor = true;
+			// 
+			// itemsLabel
+			// 
+			itemsLabel.AutoSize = true;
+			itemsLabel.Dock = DockStyle.Fill;
+			itemsLabel.Location = new Point(3, 0);
+			itemsLabel.Name = "itemsLabel";
+			itemsLabel.Size = new Size(1, 20);
+			itemsLabel.TabIndex = 2;
+			itemsLabel.Font = new Font("Microsoft Sans Serif", 8.1F*(float) Math.Pow(SfY, 0.33));
+			// 
+			// selectedLabel
+			// 
+			selectedLabel.AutoSize = true;
+			selectedLabel.Dock = DockStyle.Fill;
+			selectedLabel.Location = new Point(9, 0);
+			selectedLabel.Name = "selectedLabel";
+			selectedLabel.Size = new Size(1, (int) (20*SfY));
+			selectedLabel.TabIndex = 3;
+			selectedLabel.Font = new Font("Microsoft Sans Serif", 8.1F*(float) Math.Pow(SfY, 0.33));
+			// 
+			// mainPanel
+			// 
+			mainPanel.Dock = DockStyle.Fill;
+			mainPanel.Location = new Point(0, 0);
+			mainPanel.Margin = new Padding(0);
+			mainPanel.Name = "mainPanel";
+			mainPanel.Size = new Size(523, 518);
+			mainPanel.TabIndex = 1;
+			// 
+			// scaleFactorComboBox
+			// 
+			scaleFactorComboBox.Dock = DockStyle.Fill;
+			scaleFactorComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+			scaleFactorComboBox.Font = new Font("Microsoft Sans Serif", 7.1F*SfY);
+			scaleFactorComboBox.FormattingEnabled = true;
+			scaleFactorComboBox.Items.AddRange(new object[]{"20 %", "50 %", "70 %", "100 %", "150 %", "200 %", "400 %"});
+			scaleFactorComboBox.Location = new Point(453, 0);
+			scaleFactorComboBox.Margin = new Padding(0);
+			scaleFactorComboBox.Name = "scaleFactorComboBox";
+			scaleFactorComboBox.Size = new Size(50, 20);
+			scaleFactorComboBox.TabIndex = 4;
+			// 
+			// TableView
+			// 
+			AutoScaleDimensions = new SizeF(6F, 13F);
+			AutoScaleMode = AutoScaleMode.Font;
+			Controls.Add(tableLayoutPanel1);
+			Name = "TableView";
+			Size = new Size(523, 538);
+			tableLayoutPanel1.ResumeLayout(false);
+			tableLayoutPanel2.ResumeLayout(false);
+			tableLayoutPanel2.PerformLayout();
+			ResumeLayout(false);
 		}
 
 		public void SelectTime(double timeMs){
@@ -232,10 +400,7 @@ namespace BaseLib.Forms.Table{
 
 		private void SelectionAgentButton_OnClick(object sender, EventArgs e){
 			Point p = selectionAgentButton.PointToScreen(new Point(0, 0));
-			TableViewSelectionAgentForm w = new TableViewSelectionAgentForm(TableModel){
-				Top = p.Y - 125,
-				Left = p.X - 300
-			};
+			TableViewSelectionAgentForm w = new TableViewSelectionAgentForm(TableModel){Top = p.Y - 125, Left = p.X - 300};
 			if (w.ShowDialog() == DialogResult.OK){
 				int ind1 = w.sourceBox.SelectedIndex;
 				int ind2 = w.columnBox.SelectedIndex;
