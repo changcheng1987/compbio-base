c2eddc278e10043e46027b517528b8813382de4d
JurgenCox <cox@biochem.mpg.de>
2017-04-03 17:54:17 +0200

    some constants added

--- BaseLibS/Util/GlobalConstants.cs
+++ BaseLibS/Util/GlobalConstants.cs
@@ -1,5 +1,10 @@
-﻿namespace BaseLibS.Util{
+﻿using System;
+
+namespace BaseLibS.Util{
 	public static class GlobalConstants{
+		public const double eCharge = 1.60217646e-19; //C 
+		public const double hPlanck = 6.62606957e-34; //J*s
+		public const double hBarPlanck = hPlanck / (2 * Math.PI);
 		/// <summary>
 		/// Prefix used for reverse protein sequences.
 		/// </summary>
