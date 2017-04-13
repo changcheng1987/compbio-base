1c0f0c12f3a2348bf46ce6eda141316e9fb8c0fe
Juergen Cox <jurgen.cox@hotmail.com>
2017-04-13 18:53:26 +0200

    Making constructor public

--- BaseLibS/Num/Matrix/BoolMatrixIndexer.cs
+++ BaseLibS/Num/Matrix/BoolMatrixIndexer.cs
@@ -1,89 +1,89 @@
 ﻿using System;
 
-namespace BaseLibS.Num.Matrix{
+namespace BaseLibS.Num.Matrix {
 	[Serializable]
-	public class BoolMatrixIndexer : IBoolMatrixIndexer{
+	public class BoolMatrixIndexer : IBoolMatrixIndexer {
 		private bool[,] vals;
 
-		private BoolMatrixIndexer(bool[,] vals){
+		public BoolMatrixIndexer(bool[,] vals) {
 			this.vals = vals;
 		}
 
-		internal BoolMatrixIndexer(){}
+		public BoolMatrixIndexer() {}
 
-		public void Init(int nrows, int ncols){
+		public void Init(int nrows, int ncols) {
 			vals = new bool[nrows, ncols];
 		}
 
-		public IBoolMatrixIndexer Transpose(){
+		public IBoolMatrixIndexer Transpose() {
 			return vals == null ? new BoolMatrixIndexer() : new BoolMatrixIndexer(ArrayUtils.Transpose(vals));
 		}
 
-		public void TransposeInPlace(){
+		public void TransposeInPlace() {
 			vals = ArrayUtils.Transpose(vals);
 		}
 
-		public void Set(bool[,] value){
+		public void Set(bool[,] value) {
 			vals = value;
 		}
 
-		public bool[,] Get(){
+		public bool[,] Get() {
 			return vals;
 		}
 
-		public bool[] GetRow(int row){
+		public bool[] GetRow(int row) {
 			bool[] result = new bool[ColumnCount];
-			for (int i = 0; i < result.Length; i++){
+			for (int i = 0; i < result.Length; i++) {
 				result[i] = vals[row, i];
 			}
 			return result;
 		}
 
-		public bool[] GetColumn(int col){
+		public bool[] GetColumn(int col) {
 			bool[] result = new bool[RowCount];
-			for (int i = 0; i < result.Length; i++){
+			for (int i = 0; i < result.Length; i++) {
 				result[i] = vals[i, col];
 			}
 			return result;
 		}
 
-		public bool IsInitialized(){
+		public bool IsInitialized() {
 			return vals != null;
 		}
 
-		public void ExtractRowsInPlace(int[] rows){
-			if (vals != null){
+		public void ExtractRowsInPlace(int[] rows) {
+			if (vals != null) {
 				vals = ArrayUtils.ExtractRows(vals, rows);
 			}
 		}
 
-		public void ExtractColumnsInPlace(int[] columns){
-			if (vals != null){
+		public void ExtractColumnsInPlace(int[] columns) {
+			if (vals != null) {
 				vals = ArrayUtils.ExtractColumns(vals, columns);
 			}
 		}
 
-		public IBoolMatrixIndexer ExtractRows(int[] rows){
+		public IBoolMatrixIndexer ExtractRows(int[] rows) {
 			return new BoolMatrixIndexer(ArrayUtils.ExtractRows(vals, rows));
 		}
 
-		public IBoolMatrixIndexer ExtractColumns(int[] columns){
+		public IBoolMatrixIndexer ExtractColumns(int[] columns) {
 			return new BoolMatrixIndexer(ArrayUtils.ExtractColumns(vals, columns));
 		}
 
 		public int RowCount => vals.GetLength(0);
 		public int ColumnCount => vals.GetLength(1);
 
-		public bool this[int i, int j]{
+		public bool this[int i, int j] {
 			get { return vals[i, j]; }
 			set { vals[i, j] = value; }
 		}
 
-		public void Dispose(){
+		public void Dispose() {
 			vals = null;
 		}
 
-		public object Clone(){
+		public object Clone() {
 			return vals == null ? new BoolMatrixIndexer(null) : new BoolMatrixIndexer((bool[,]) vals.Clone());
 		}
 	}
