ce61b1e4c611edf3b4f602ba9ea730ba721cb335
Juergen Cox <jurgen.cox@hotmail.com>
2017-06-16 14:04:53 +0200

    minor

--- BaseLib/Forms/FileParameterControl.Designer.cs
+++ BaseLib/Forms/FileParameterControl.Designer.cs
@@ -38,7 +38,7 @@
 			// 
 			this.tableLayoutPanel1.ColumnCount = 2;
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 187F));
 			this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
 			this.tableLayoutPanel1.Controls.Add(this.button1, 1, 0);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -47,7 +47,7 @@
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
 			this.tableLayoutPanel1.RowCount = 1;
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(579, 42);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(1544, 100);
 			this.tableLayoutPanel1.TabIndex = 0;
 			// 
 			// textBox1
@@ -56,28 +56,28 @@
 			this.textBox1.Location = new System.Drawing.Point(0, 0);
 			this.textBox1.Margin = new System.Windows.Forms.Padding(0);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(509, 20);
+			this.textBox1.Size = new System.Drawing.Size(1357, 38);
 			this.textBox1.TabIndex = 0;
 			// 
 			// button1
 			// 
 			this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.button1.Location = new System.Drawing.Point(509, 0);
+			this.button1.Location = new System.Drawing.Point(1357, 0);
 			this.button1.Margin = new System.Windows.Forms.Padding(0);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(70, 42);
+			this.button1.Size = new System.Drawing.Size(187, 100);
 			this.button1.TabIndex = 1;
 			this.button1.Text = "Select";
 			this.button1.UseVisualStyleBackColor = true;
 			// 
 			// FileParameterControl
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Margin = new System.Windows.Forms.Padding(0);
 			this.Name = "FileParameterControl";
-			this.Size = new System.Drawing.Size(579, 42);
+			this.Size = new System.Drawing.Size(1544, 100);
 			this.tableLayoutPanel1.ResumeLayout(false);
 			this.tableLayoutPanel1.PerformLayout();
 			this.ResumeLayout(false);
