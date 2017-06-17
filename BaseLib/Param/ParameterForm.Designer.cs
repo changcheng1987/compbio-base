9c9e78d6174268f0edb245ad4baa84185f55414c
Juergen Cox <jurgen.cox@hotmail.com>
2017-06-17 19:44:57 +0200

    additional button row on top of parameter panel

--- BaseLib/Param/ParameterForm.Designer.cs
+++ BaseLib/Param/ParameterForm.Designer.cs
@@ -29,13 +29,19 @@
 		private void InitializeComponent()
 		{
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+			this.cancelButton2 = new System.Windows.Forms.Button();
+			this.helpButton2 = new System.Windows.Forms.Button();
+			this.okButton2 = new System.Windows.Forms.Button();
+			this.helpPanel2 = new System.Windows.Forms.Panel();
 			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-			this.cancelButton = new System.Windows.Forms.Button();
-			this.helpButton = new System.Windows.Forms.Button();
-			this.okButton = new System.Windows.Forms.Button();
-			this.helpPanel = new System.Windows.Forms.Panel();
-			this.parameterPanel1 = new ParameterPanel();
+			this.cancelButton1 = new System.Windows.Forms.Button();
+			this.helpButton1 = new System.Windows.Forms.Button();
+			this.okButton1 = new System.Windows.Forms.Button();
+			this.helpPanel1 = new System.Windows.Forms.Panel();
+			this.parameterPanel1 = new BaseLib.Param.ParameterPanel();
 			this.tableLayoutPanel1.SuspendLayout();
+			this.tableLayoutPanel3.SuspendLayout();
 			this.tableLayoutPanel2.SuspendLayout();
 			this.SuspendLayout();
 			// 
@@ -43,79 +49,143 @@
 			// 
 			this.tableLayoutPanel1.ColumnCount = 1;
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
-			this.tableLayoutPanel1.Controls.Add(this.parameterPanel1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.parameterPanel1, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
 			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 53F));
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(1110, 566);
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 53F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(2688, 1350);
 			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// tableLayoutPanel3
+			// 
+			this.tableLayoutPanel3.ColumnCount = 4;
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 187F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 67F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 187F));
+			this.tableLayoutPanel3.Controls.Add(this.cancelButton2, 0, 0);
+			this.tableLayoutPanel3.Controls.Add(this.helpButton2, 2, 0);
+			this.tableLayoutPanel3.Controls.Add(this.okButton2, 3, 0);
+			this.tableLayoutPanel3.Controls.Add(this.helpPanel2, 1, 0);
+			this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
+			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+			this.tableLayoutPanel3.RowCount = 1;
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(2688, 53);
+			this.tableLayoutPanel3.TabIndex = 0;
+			// 
+			// cancelButton2
+			// 
+			this.cancelButton2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.cancelButton2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.cancelButton2.Location = new System.Drawing.Point(0, 0);
+			this.cancelButton2.Margin = new System.Windows.Forms.Padding(0);
+			this.cancelButton2.Name = "cancelButton2";
+			this.cancelButton2.Size = new System.Drawing.Size(187, 53);
+			this.cancelButton2.TabIndex = 2;
+			this.cancelButton2.Text = "Cancel";
+			this.cancelButton2.UseVisualStyleBackColor = true;
+			// 
+			// helpButton2
+			// 
+			this.helpButton2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.helpButton2.Location = new System.Drawing.Point(2434, 0);
+			this.helpButton2.Margin = new System.Windows.Forms.Padding(0);
+			this.helpButton2.Name = "helpButton2";
+			this.helpButton2.Size = new System.Drawing.Size(67, 53);
+			this.helpButton2.TabIndex = 1;
+			this.helpButton2.UseVisualStyleBackColor = true;
+			// 
+			// okButton2
+			// 
+			this.okButton2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.okButton2.Location = new System.Drawing.Point(2501, 0);
+			this.okButton2.Margin = new System.Windows.Forms.Padding(0);
+			this.okButton2.Name = "okButton2";
+			this.okButton2.Size = new System.Drawing.Size(187, 53);
+			this.okButton2.TabIndex = 0;
+			this.okButton2.Text = "OK";
+			this.okButton2.UseVisualStyleBackColor = true;
+			// 
+			// helpPanel2
+			// 
+			this.helpPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.helpPanel2.Location = new System.Drawing.Point(187, 0);
+			this.helpPanel2.Margin = new System.Windows.Forms.Padding(0);
+			this.helpPanel2.Name = "helpPanel2";
+			this.helpPanel2.Size = new System.Drawing.Size(2247, 53);
+			this.helpPanel2.TabIndex = 3;
 			// 
 			// tableLayoutPanel2
 			// 
 			this.tableLayoutPanel2.ColumnCount = 4;
-			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 187F));
 			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 25F));
-			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
-			this.tableLayoutPanel2.Controls.Add(this.cancelButton, 0, 0);
-			this.tableLayoutPanel2.Controls.Add(this.helpButton, 2, 0);
-			this.tableLayoutPanel2.Controls.Add(this.okButton, 3, 0);
-			this.tableLayoutPanel2.Controls.Add(this.helpPanel, 1, 0);
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 67F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 187F));
+			this.tableLayoutPanel2.Controls.Add(this.cancelButton1, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.helpButton1, 2, 0);
+			this.tableLayoutPanel2.Controls.Add(this.okButton1, 3, 0);
+			this.tableLayoutPanel2.Controls.Add(this.helpPanel1, 1, 0);
 			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 542);
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 1297);
 			this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
 			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
 			this.tableLayoutPanel2.RowCount = 1;
 			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel2.Size = new System.Drawing.Size(1110, 24);
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(2688, 53);
 			this.tableLayoutPanel2.TabIndex = 0;
 			// 
-			// cancelButton
+			// cancelButton1
 			// 
-			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.cancelButton.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.cancelButton.Location = new System.Drawing.Point(0, 0);
-			this.cancelButton.Margin = new System.Windows.Forms.Padding(0);
-			this.cancelButton.Name = "cancelButton";
-			this.cancelButton.Size = new System.Drawing.Size(70, 24);
-			this.cancelButton.TabIndex = 2;
-			this.cancelButton.Text = "Cancel";
-			this.cancelButton.UseVisualStyleBackColor = true;
+			this.cancelButton1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.cancelButton1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.cancelButton1.Location = new System.Drawing.Point(0, 0);
+			this.cancelButton1.Margin = new System.Windows.Forms.Padding(0);
+			this.cancelButton1.Name = "cancelButton1";
+			this.cancelButton1.Size = new System.Drawing.Size(187, 53);
+			this.cancelButton1.TabIndex = 2;
+			this.cancelButton1.Text = "Cancel";
+			this.cancelButton1.UseVisualStyleBackColor = true;
 			// 
-			// helpButton
+			// helpButton1
 			// 
-			this.helpButton.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.helpButton.Location = new System.Drawing.Point(1015, 0);
-			this.helpButton.Margin = new System.Windows.Forms.Padding(0);
-			this.helpButton.Name = "helpButton";
-			this.helpButton.Size = new System.Drawing.Size(25, 24);
-			this.helpButton.TabIndex = 1;
-			this.helpButton.UseVisualStyleBackColor = true;
+			this.helpButton1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.helpButton1.Location = new System.Drawing.Point(2434, 0);
+			this.helpButton1.Margin = new System.Windows.Forms.Padding(0);
+			this.helpButton1.Name = "helpButton1";
+			this.helpButton1.Size = new System.Drawing.Size(67, 53);
+			this.helpButton1.TabIndex = 1;
+			this.helpButton1.UseVisualStyleBackColor = true;
 			// 
-			// okButton
+			// okButton1
 			// 
-			this.okButton.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.okButton.Location = new System.Drawing.Point(1040, 0);
-			this.okButton.Margin = new System.Windows.Forms.Padding(0);
-			this.okButton.Name = "okButton";
-			this.okButton.Size = new System.Drawing.Size(70, 24);
-			this.okButton.TabIndex = 0;
-			this.okButton.Text = "OK";
-			this.okButton.UseVisualStyleBackColor = true;
+			this.okButton1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.okButton1.Location = new System.Drawing.Point(2501, 0);
+			this.okButton1.Margin = new System.Windows.Forms.Padding(0);
+			this.okButton1.Name = "okButton1";
+			this.okButton1.Size = new System.Drawing.Size(187, 53);
+			this.okButton1.TabIndex = 0;
+			this.okButton1.Text = "OK";
+			this.okButton1.UseVisualStyleBackColor = true;
 			// 
-			// helpPanel
+			// helpPanel1
 			// 
-			this.helpPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.helpPanel.Location = new System.Drawing.Point(70, 0);
-			this.helpPanel.Margin = new System.Windows.Forms.Padding(0);
-			this.helpPanel.Name = "helpPanel";
-			this.helpPanel.Size = new System.Drawing.Size(945, 24);
-			this.helpPanel.TabIndex = 3;
+			this.helpPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.helpPanel1.Location = new System.Drawing.Point(187, 0);
+			this.helpPanel1.Margin = new System.Windows.Forms.Padding(0);
+			this.helpPanel1.Name = "helpPanel1";
+			this.helpPanel1.Size = new System.Drawing.Size(2247, 53);
+			this.helpPanel1.TabIndex = 3;
 			// 
 			// parameterPanel1
 			// 
@@ -123,23 +193,25 @@
 			this.parameterPanel1.CollapsedDefault = false;
 			this.parameterPanel1.Collapsible = true;
 			this.parameterPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.parameterPanel1.Location = new System.Drawing.Point(0, 0);
+			this.parameterPanel1.Location = new System.Drawing.Point(0, 53);
 			this.parameterPanel1.Margin = new System.Windows.Forms.Padding(0);
 			this.parameterPanel1.Name = "parameterPanel1";
-			this.parameterPanel1.Size = new System.Drawing.Size(1110, 542);
+			this.parameterPanel1.Size = new System.Drawing.Size(2688, 1244);
 			this.parameterPanel1.TabIndex = 1;
 			// 
 			// ParameterForm
 			// 
-			this.AcceptButton = this.okButton;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AcceptButton = this.okButton1;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.CancelButton = this.cancelButton;
-			this.ClientSize = new System.Drawing.Size(1110, 566);
+			this.CancelButton = this.cancelButton1;
+			this.ClientSize = new System.Drawing.Size(2688, 1350);
 			this.Controls.Add(this.tableLayoutPanel1);
+			this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
 			this.Name = "ParameterForm";
 			this.Text = "Parameters";
 			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel3.ResumeLayout(false);
 			this.tableLayoutPanel2.ResumeLayout(false);
 			this.ResumeLayout(false);
 
@@ -149,10 +221,15 @@
 
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-		private System.Windows.Forms.Button cancelButton;
-		private System.Windows.Forms.Button helpButton;
-		private System.Windows.Forms.Button okButton;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+		private System.Windows.Forms.Button cancelButton1;
+		private System.Windows.Forms.Button cancelButton2;
+		private System.Windows.Forms.Button helpButton1;
+		private System.Windows.Forms.Button helpButton2;
+		private System.Windows.Forms.Button okButton1;
+		private System.Windows.Forms.Button okButton2;
 		private ParameterPanel parameterPanel1;
-		private System.Windows.Forms.Panel helpPanel;
+		private System.Windows.Forms.Panel helpPanel1;
+		private System.Windows.Forms.Panel helpPanel2;
 	}
 }
--- BaseLib/Param/ParameterForm.cs
+++ BaseLib/Param/ParameterForm.cs
@@ -18,15 +18,22 @@ namespace BaseLib.Param{
 			InitializeComponent();
 			this.helpAction = helpAction;
 			if (helpAction == null){
-				helpButton.Width = 0;
+				helpButton1.Width = 0;
+				helpButton2.Width = 0;
 			}
-			okButton.Click += OkButtonClick;
-			cancelButton.Click += CancelButtonClick;
-			helpButton.Click += HelpButtonClick;
-			helpButton.Image = new Bitmap(GraphUtils.ToBitmap(Bitmap2.GetImage("help.png")), 18, 18);
+			okButton1.Click += OkButtonClick;
+			okButton2.Click += OkButtonClick;
+			cancelButton1.Click += CancelButtonClick;
+			cancelButton2.Click += CancelButtonClick;
+			helpButton1.Click += HelpButtonClick;
+			helpButton2.Click += HelpButtonClick;
+			Bitmap bm = new Bitmap(GraphUtils.ToBitmap(Bitmap2.GetImage("help.png")), 18, 18);
+			helpButton1.Image = bm;
+			helpButton2.Image = bm;
 			KeyDown += OnKeyDownHandler;
 			Height = (int) (parameterPanel1.Init(parameters) + 65);
-			helpPanel.Controls.Clear();
+			helpPanel1.Controls.Clear();
+			helpPanel2.Controls.Clear();
 			List<string> titles = new List<string>();
 			List<string> description = new List<string>();
 			if (!string.IsNullOrEmpty(helpDescription)){
@@ -49,14 +56,19 @@ namespace BaseLib.Param{
 					}
 				}
 			}
+			AddHelp(titles,description, title, helpPanel1);
+			AddHelp(titles,description, title, helpPanel2);
+		}
+
+		private void AddHelp(List<string> titles, List<string> description, string title, Panel helpPanel) {
 			TableLayoutPanel g = new TableLayoutPanel();
 			g.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
-			for (int i = 0; i < titles.Count; i++){
+			for (int i = 0; i < titles.Count; i++) {
 				g.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize, 50));
 			}
 			g.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
-			for (int i = 0; i < titles.Count; i++){
-				Label tb = new Label{Text = titles[i]};
+			for (int i = 0; i < titles.Count; i++) {
+				Label tb = new Label { Text = titles[i] };
 				//ToolTipService.SetShowDuration(tb, 400000);
 				//if (description[i] != null){
 				//	tb.ToolTip = new ToolTip{
@@ -75,8 +87,7 @@ namespace BaseLib.Param{
 		}
 
 		public void FocusOkButton(){
-			//Invoke(new Action(delegate { okButton.Focus(); }));
-			okButton.Focus();
+			okButton1.Focus();
 		}
 
 		private void OkButtonClick(object sender, EventArgs e){
