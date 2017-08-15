05b68bc454e5f2b65ae4f7f09b55d3a0b570032e
Juergen Cox <jurgen.cox@hotmail.com>
2017-08-15 14:32:07 +0200

    invariant culture

--- BaseLib/Query/DoubleQueryForm.cs
+++ BaseLib/Query/DoubleQueryForm.cs
@@ -1,33 +1,36 @@
 ﻿using System;
+using System.Globalization;
 using System.Windows.Forms;
 
-namespace BaseLib.Query{
-	public partial class DoubleQueryForm : Form{
-		public DoubleQueryForm(double value){
+namespace BaseLib.Query {
+	public partial class DoubleQueryForm : Form {
+		public DoubleQueryForm(double value) {
 			InitializeComponent();
 			StartPosition = FormStartPosition.Manual;
 			okButton.Click += OkButtonOnClick;
 			cancelButton.Click += CancelButtonOnClick;
-			textBox1.Text = "" + value;
+			textBox1.Text = value.ToString(CultureInfo.InvariantCulture);
 			textBox1.KeyDown += TextBox1OnKeyDown;
 			ActiveControl = textBox1;
 		}
 
-		public double Value => double.Parse(textBox1.Text);
+		public double Value => double.TryParse(textBox1.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out double val)
+			? val
+			: double.NaN;
 
-		private void TextBox1OnKeyDown(object sender, KeyEventArgs keyEventArgs){
-			if (keyEventArgs.KeyCode == Keys.Return){
+		private void TextBox1OnKeyDown(object sender, KeyEventArgs keyEventArgs) {
+			if (keyEventArgs.KeyCode == Keys.Return) {
 				DialogResult = DialogResult.OK;
 				Close();
 			}
 		}
 
-		private void CancelButtonOnClick(object sender, EventArgs eventArgs){
+		private void CancelButtonOnClick(object sender, EventArgs eventArgs) {
 			DialogResult = DialogResult.Cancel;
 			Close();
 		}
 
-		private void OkButtonOnClick(object sender, EventArgs eventArgs){
+		private void OkButtonOnClick(object sender, EventArgs eventArgs) {
 			DialogResult = DialogResult.OK;
 			Close();
 		}
