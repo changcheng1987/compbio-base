d9f627a64196d6244df350aa75c8348aa0bfa4ba
rudolph <rudolph@biochem.mpg.de>
2017-08-31 16:26:21 +0200

    fallback to default font for pdf graphics

--- BaseLib/BaseLib.Test/FontTest.cs
+++ BaseLib/BaseLib.Test/FontTest.cs
@@ -17,5 +17,14 @@ namespace BaseLib.Test
             Assert.AreEqual(nativeFont.Name, convertedFont.Name);
             Assert.AreEqual(nativeFont.Size, convertedFont.Size);
         }
+
+        [TestMethod]
+        public void TestArialUnicode()
+        {
+            var font2 = new Font2("Arial Unicode MS", 9, FontStyle2.Regular);
+            var font = PdfGraphicUtils.GetFont(font2);
+            Assert.IsNotNull(font.BaseFont);
+            var convertedFont = GraphUtils.ToFont(font2);
+        }
     }
 }
