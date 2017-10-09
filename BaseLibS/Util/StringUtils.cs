4fa0c52abfebefb5ec3dfe0dda74bb3274824ffa
Juergen Cox <jurgen.cox@hotmail.com>
2017-10-09 18:42:29 +0200

    Some default column names added

--- BaseLibS/Util/StringUtils.cs
+++ BaseLibS/Util/StringUtils.cs
@@ -832,7 +832,7 @@ namespace BaseLibS.Util {
 			"names", "proteins", "positions within proteins", "leading proteins", "leading razor protein", "md sequence",
 			"md proteins", "md gene names", "md protein names", "dp base sequence", "dp probabilities", "dp proteins",
 			"dp gene names", "dp protein names", "name", "dn sequence", "title", "volume", "number", "pages",
-			"modified sequence", "formula", "formula2"
+			"modified sequence", "formula", "formula2", "geneid", "chr"
 		});
 
 		public static readonly HashSet<string> numericColDefaultNames = new HashSet<string>(new[] {
@@ -868,7 +868,7 @@ namespace BaseLibS.Util {
 			"max frame number", "ion mobility index", "ion mobility index length", "ion mobility index length (fwhm)",
 			"isotope correlation", "peptides", "razor + unique peptides", "unique peptides", "sequence coverage [%]",
 			"unique sequence coverage [%]", "unique + razor sequence coverage [%]", "mol. weight [kda]", "dm [mda]", "dm [ppm]",
-			"time [sec]", "du", "isotope index"
+			"time [sec]", "du", "isotope index", "start", "end"
 		});
 
 		public static readonly HashSet<string> multiNumericColDefaultNames = new HashSet<string>(new[]
