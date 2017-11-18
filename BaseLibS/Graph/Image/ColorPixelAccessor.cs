d5667db32ae301855daacc48b5d02a37e7121ffb
Juergen Cox <jurgen.cox@hotmail.com>
2017-11-18 20:31:55 +0100

    removing unsafe code

--- BaseLibS/Graph/Image/ColorPixelAccessor.cs
+++ BaseLibS/Graph/Image/ColorPixelAccessor.cs
@@ -1,41 +1,39 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 
-namespace BaseLibS.Graph.Image{
-	public sealed unsafe class ColorPixelAccessor : IPixelAccessor{
-		private Color2* pixelsBase;
-		private GCHandle pixelsHandle;
+namespace BaseLibS.Graph.Image {
+	public sealed class ColorPixelAccessor : IPixelAccessor {
+		private Color2[] pixelsBase;
 		private bool isDisposed;
-		public ColorPixelAccessor(IImageBase image){
-			if (image == null){
+
+		public ColorPixelAccessor(IImageBase image) {
+			if (image == null) {
 				throw new ArgumentNullException();
 			}
-			if (image.Width <= 0 || image.Height <= 0){
+			if (image.Width <= 0 || image.Height <= 0) {
 				throw new ArgumentOutOfRangeException();
 			}
 			Width = image.Width;
 			Height = image.Height;
-			pixelsHandle = GCHandle.Alloc(((ImageBase) image).Pixels, GCHandleType.Pinned);
-			pixelsBase = (Color2*) pixelsHandle.AddrOfPinnedObject().ToPointer();
+			pixelsBase = ((ImageBase) image).Pixels;
 		}
-		~ColorPixelAccessor(){
+
+		~ColorPixelAccessor() {
 			Dispose();
 		}
+
 		public int Width { get; }
 		public int Height { get; }
-		public Color2 this[int x, int y]{
-			get => *(pixelsBase + (y*Width + x));
-			set => *(pixelsBase + (y*Width + x)) = value;
+
+		public Color2 this[int x, int y] {
+			get => pixelsBase[y * Width + x];
+			set => pixelsBase[y * Width + x] = value;
 		}
-		public void Dispose(){
-			if (isDisposed){
+
+		public void Dispose() {
+			if (isDisposed) {
 				return;
 			}
-			if (pixelsHandle.IsAllocated){
-				pixelsHandle.Free();
-			}
 			pixelsBase = null;
-
 			isDisposed = true;
 			GC.SuppressFinalize(this);
 		}
