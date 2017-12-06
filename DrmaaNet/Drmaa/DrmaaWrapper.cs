1f8e977fea99ac9372a56ea21775c92348c12a3f
nikita prianichinkov <lpenguin@yandex.ru>
2017-12-06 15:52:27 +0100

    Moved DrmaaWrapperInternal to separate file

--- DrmaaNet/Drmaa/DrmaaWrapper.cs
+++ DrmaaNet/Drmaa/DrmaaWrapper.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Drmaa
@@ -19,123 +18,6 @@ namespace Drmaa
         public override string ToString(){
             return $"code: {code}, message: \"{message}\"";
         }
-    }
-
-    internal class DrmaaWrapperIntenal
-    {
-        public static readonly int NO_MORE_ELEMENTS = 25;
-        public static readonly int DRMAA_CONTROL_SUSPEND = 0;
-        public static readonly int DRMAA_CONTROL_RESUME = 1;
-        public static readonly int DRMAA_CONTROL_HOLD = 2;
-        public static readonly int DRMAA_CONTROL_RELEASE = 3;
-        public static readonly int DRMAA_CONTROL_TERMINATE = 4;
-
-        public static int  DRMAA_PS_UNDETERMINED = 0x00;
-        public static int  DRMAA_PS_QUEUED_ACTIVE = 0x10;
-        public static int  DRMAA_PS_SYSTEM_ON_HOLD = 0x11;
-        public static int  DRMAA_PS_USER_ON_HOLD = 0x12;
-        public static int  DRMAA_PS_USER_SYSTEM_ON_HOLD = 0x13;
-        public static int  DRMAA_PS_RUNNING = 0x20;
-        public static int  DRMAA_PS_SYSTEM_SUSPENDED = 0x21;
-        public static int  DRMAA_PS_USER_SUSPENDED = 0x22;
-        public static int  DRMAA_PS_USER_SYSTEM_SUSPENDED = 0x23;
-        public static int  DRMAA_PS_DONE = 0x30;
-        public static int  DRMAA_PS_FAILED = 0x40;
-
-
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_init(String contact, StringBuilder error_diagnosis, int error_diag_len);
-
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_exit(StringBuilder error_diagnosis, int error_diag_len);
-
-
-        
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_allocate_job_template(out IntPtr jt, StringBuilder error_diagnosis, int error_diag_len);
-
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_get_attribute_names(
-                                    out IntPtr values,
-                                    StringBuilder error_diagnosis, int error_diag_len
-                                    );
-
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_get_next_attr_name(
-            IntPtr values,
-            StringBuilder value, int value_len);
-
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_get_attribute(
-            IntPtr jt,
-            string name, StringBuilder value, int value_len,
-            StringBuilder error_diagnosis, int error_diag_len
-            );
-
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_set_attribute(
-            IntPtr jt,
-            string name, string value,
-            StringBuilder error_diagnosis, int error_diag_len
-            );
-
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_run_job(
-            StringBuilder job_id, int job_id_len, IntPtr jt,
-            StringBuilder error_diagnosis, int error_diag_len
-        );
-
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_control(
-            string job_id, int action,
-            StringBuilder error_diagnosis, int error_diag_len
-        );
-
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_job_ps(
-            string job_id, out int remote_ps,
-            StringBuilder error_diagnosis, int error_diag_len
-        );
-
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_set_vector_attribute(
-            IntPtr jt,
-            string name, string[] values,
-            StringBuilder error_diagnosis, int error_diag_len
-        );
-
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_get_vector_attribute(
-            IntPtr jt,
-            string name, out IntPtr values,
-            StringBuilder error_diagnosis, int error_diag_len
-        );
-
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_get_next_attr_value( 
-            IntPtr values,
-            StringBuilder value, int value_len 
-        );
-
-        [DllImport("libdrmaa")]
-        public static extern void drmaa_release_attr_values( 
-            IntPtr values 
-        );
-        
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_wait(
-            string job_id,
-            StringBuilder job_id_out, int job_id_out_len,
-            out int stat, long timeout, out IntPtr rusage,
-            StringBuilder error_diagnosis, int error_diag_len
-        );
-        
-        [DllImport("libdrmaa")]
-        public static extern int drmaa_wexitstatus( 
-            out int exit_status, int stat,
-            StringBuilder error_diagnosis, int error_diag_len
-        );
-
     }
 
     public struct DrmaaJobTemplate
@@ -166,7 +48,7 @@ namespace Drmaa
         Failed = 0x40,
     }
     
-    internal class Attributes {
+    internal static class Attributes {
         internal static readonly string RemoteCommand = "drmaa_remote_command";
         internal static readonly string WorkingDirectory = "drmaa_wd";
         internal static readonly string NativeSpecification = "drmaa_native_specification";
@@ -180,12 +62,12 @@ namespace Drmaa
         internal static readonly string JobEnvironment = "drmaa_v_env";
     };
 
-    public class DrmaaWrapper
+    internal static class DrmaaWrapper
     {
         public static void Init(String contact)
         {
             StringBuilder error = new StringBuilder(1024);
-            int res = DrmaaWrapperIntenal.drmaa_init(contact, error, error.Capacity);
+            int res = DrmaaWrapperInternal.drmaa_init(contact, error, error.Capacity);
             if (res != 0)
             {
                 throw new DrmaaException(res, error.ToString());
@@ -195,7 +77,7 @@ namespace Drmaa
         public static void Exit(String contact)
         {
             StringBuilder error = new StringBuilder(1024);
-            int res = DrmaaWrapperIntenal.drmaa_exit(error, error.Capacity);
+            int res = DrmaaWrapperInternal.drmaa_exit(error, error.Capacity);
             if (res != 0)
             {
                 throw new DrmaaException(res, error.ToString());
@@ -206,7 +88,7 @@ namespace Drmaa
         {
             IntPtr instance;
             StringBuilder error = new StringBuilder(1024);
-            int res = DrmaaWrapperIntenal.drmaa_allocate_job_template(out instance, error, error.Capacity);
+            int res = DrmaaWrapperInternal.drmaa_allocate_job_template(out instance, error, error.Capacity);
             if (res != 0)
             {
                 throw new DrmaaException(res, error.ToString());
@@ -219,7 +101,7 @@ namespace Drmaa
             StringBuilder value = new StringBuilder(1024);
 
             StringBuilder error = new StringBuilder(1024);
-            int res = DrmaaWrapperIntenal.drmaa_get_attribute(
+            int res = DrmaaWrapperInternal.drmaa_get_attribute(
                 jobTemplate.instance,
                 name,
                 value,
@@ -236,7 +118,7 @@ namespace Drmaa
         public static void SetAttribute(DrmaaJobTemplate jobTemplate, string name, string value)
         {
             StringBuilder error = new StringBuilder(1024);
-            int res = DrmaaWrapperIntenal.drmaa_set_attribute(
+            int res = DrmaaWrapperInternal.drmaa_set_attribute(
                 jobTemplate.instance,
                 name,
                 value,
@@ -254,7 +136,7 @@ namespace Drmaa
             StringBuilder jobId = new StringBuilder(1024);
 
             StringBuilder error = new StringBuilder(1024);
-            int res = DrmaaWrapperIntenal.drmaa_run_job(
+            int res = DrmaaWrapperInternal.drmaa_run_job(
                 jobId,
                 jobId.Capacity,
                 jobTemplate.instance,
@@ -269,7 +151,7 @@ namespace Drmaa
 
         public static void Control(string jobId, Action action){
             StringBuilder error = new StringBuilder(1024);
-            int res = DrmaaWrapperIntenal.drmaa_control(
+            int res = DrmaaWrapperInternal.drmaa_control(
                 jobId, (int)action,
                 error, error.Capacity
             );
@@ -283,7 +165,7 @@ namespace Drmaa
             StringBuilder error = new StringBuilder(1024);
             int status;
 
-            int res = DrmaaWrapperIntenal.drmaa_job_ps(
+            int res = DrmaaWrapperInternal.drmaa_job_ps(
                 jobId, out status,
                 error, error.Capacity
             );
@@ -297,7 +179,7 @@ namespace Drmaa
         public static string[] GetAttributes(DrmaaJobTemplate jobTemplate, string name){
             IntPtr valuesIter;
             StringBuilder error = new StringBuilder(1024);
-            int res = DrmaaWrapperIntenal.drmaa_get_vector_attribute(
+            int res = DrmaaWrapperInternal.drmaa_get_vector_attribute(
                 jobTemplate.instance,
                 name,
                 out valuesIter,
@@ -313,7 +195,7 @@ namespace Drmaa
             var result = new List<string>();
             while(true)
             {
-                res = DrmaaWrapperIntenal.drmaa_get_next_attr_value(
+                res = DrmaaWrapperInternal.drmaa_get_next_attr_value(
                     valuesIter,
                     value,
                     value.Capacity
@@ -324,14 +206,14 @@ namespace Drmaa
                 }
                 result.Add(value.ToString());
             }
-            DrmaaWrapperIntenal.drmaa_release_attr_values(valuesIter);
+            DrmaaWrapperInternal.drmaa_release_attr_values(valuesIter);
             return result.ToArray();
         }
 
         public static void SetAttributes(DrmaaJobTemplate jobTemplate, string name, string[] values){
             StringBuilder error = new StringBuilder(1024);
 
-            int res = DrmaaWrapperIntenal.drmaa_set_vector_attribute(
+            int res = DrmaaWrapperInternal.drmaa_set_vector_attribute(
                 jobTemplate.instance,
                 name,
                 values,
@@ -358,7 +240,7 @@ namespace Drmaa
 
             int stat;
             IntPtr rusage;
-            int res = DrmaaWrapperIntenal.drmaa_wait(
+            int res = DrmaaWrapperInternal.drmaa_wait(
                 jobId,
                 jobIdOut, jobIdOut.Capacity,
                 out stat, timeout, out rusage,
@@ -369,7 +251,7 @@ namespace Drmaa
                 throw new DrmaaException(res, error.ToString());
             }
             int exitStatus;
-            res = DrmaaWrapperIntenal.drmaa_wexitstatus(
+            res = DrmaaWrapperInternal.drmaa_wexitstatus(
                 out exitStatus, stat,
                 error, error.Capacity  
             );
---
+++ DrmaaNet/Drmaa/DrmaaWrapperInternal.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Drmaa
+{
+    internal static class DrmaaWrapperInternal
+    {
+        public static readonly int NO_MORE_ELEMENTS = 25;
+        public static readonly int DRMAA_CONTROL_SUSPEND = 0;
+        public static readonly int DRMAA_CONTROL_RESUME = 1;
+        public static readonly int DRMAA_CONTROL_HOLD = 2;
+        public static readonly int DRMAA_CONTROL_RELEASE = 3;
+        public static readonly int DRMAA_CONTROL_TERMINATE = 4;
+
+        public static int  DRMAA_PS_UNDETERMINED = 0x00;
+        public static int  DRMAA_PS_QUEUED_ACTIVE = 0x10;
+        public static int  DRMAA_PS_SYSTEM_ON_HOLD = 0x11;
+        public static int  DRMAA_PS_USER_ON_HOLD = 0x12;
+        public static int  DRMAA_PS_USER_SYSTEM_ON_HOLD = 0x13;
+        public static int  DRMAA_PS_RUNNING = 0x20;
+        public static int  DRMAA_PS_SYSTEM_SUSPENDED = 0x21;
+        public static int  DRMAA_PS_USER_SUSPENDED = 0x22;
+        public static int  DRMAA_PS_USER_SYSTEM_SUSPENDED = 0x23;
+        public static int  DRMAA_PS_DONE = 0x30;
+        public static int  DRMAA_PS_FAILED = 0x40;
+
+
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_init(String contact, StringBuilder error_diagnosis, int error_diag_len);
+
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_exit(StringBuilder error_diagnosis, int error_diag_len);
+
+
+        
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_allocate_job_template(out IntPtr jt, StringBuilder error_diagnosis, int error_diag_len);
+
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_get_attribute_names(
+            out IntPtr values,
+            StringBuilder error_diagnosis, int error_diag_len
+        );
+
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_get_next_attr_name(
+            IntPtr values,
+            StringBuilder value, int value_len);
+
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_get_attribute(
+            IntPtr jt,
+            string name, StringBuilder value, int value_len,
+            StringBuilder error_diagnosis, int error_diag_len
+        );
+
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_set_attribute(
+            IntPtr jt,
+            string name, string value,
+            StringBuilder error_diagnosis, int error_diag_len
+        );
+
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_run_job(
+            StringBuilder job_id, int job_id_len, IntPtr jt,
+            StringBuilder error_diagnosis, int error_diag_len
+        );
+
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_control(
+            string job_id, int action,
+            StringBuilder error_diagnosis, int error_diag_len
+        );
+
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_job_ps(
+            string job_id, out int remote_ps,
+            StringBuilder error_diagnosis, int error_diag_len
+        );
+
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_set_vector_attribute(
+            IntPtr jt,
+            string name, string[] values,
+            StringBuilder error_diagnosis, int error_diag_len
+        );
+
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_get_vector_attribute(
+            IntPtr jt,
+            string name, out IntPtr values,
+            StringBuilder error_diagnosis, int error_diag_len
+        );
+
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_get_next_attr_value( 
+            IntPtr values,
+            StringBuilder value, int value_len 
+        );
+
+        [DllImport("libdrmaa")]
+        public static extern void drmaa_release_attr_values( 
+            IntPtr values 
+        );
+        
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_wait(
+            string job_id,
+            StringBuilder job_id_out, int job_id_out_len,
+            out int stat, long timeout, out IntPtr rusage,
+            StringBuilder error_diagnosis, int error_diag_len
+        );
+        
+        [DllImport("libdrmaa")]
+        public static extern int drmaa_wexitstatus( 
+            out int exit_status, int stat,
+            StringBuilder error_diagnosis, int error_diag_len
+        );
+
+    }
+}
