8efd450e2d01cb18230445b59cb300cfe6f61835
rudolph <rudolph@biochem.mpg.de>
2017-12-15 15:52:37 +0100

    Document fisher exact test functions

--- BaseLibS/Num/Test/FisherExactTest.cs
+++ BaseLibS/Num/Test/FisherExactTest.cs
@@ -3,19 +3,39 @@ using BaseLibS.Num.Func;
 
 namespace BaseLibS.Num.Test{
 	public class FisherExactTest{
+
+        /// <summary>
+        /// P-value calculation from q_xy of the contingency table. See <see cref="GetLogFisherP"/> for log p-value
+        /// and <see cref="CalcContingency"/> for calculating the contingency table.
+        /// </summary>
+        /// <param name="q00"></param>
+        /// <param name="q01"></param>
+        /// <param name="q10"></param>
+        /// <param name="q11"></param>
+        /// <returns></returns>
 		public static double Test(int q00, int q01, int q10, int q11){
 			return Math.Exp(GetLogFisherP(q00, q01, q10, q11));
 		}
 
+        /// <summary>
+        /// Calculate contingency table and p-value, see <see cref="CalcContingency"/>.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
 		public static double Test(bool[] x, bool[] y){
-			int q00;
-			int q01;
-			int q10;
-			int q11;
-			CalcContingency(x, y, out q00, out q01, out q10, out q11);
-			return Test(q00, q01, q10, q11);
+            CalcContingency(x, y, out int q00, out int q01, out int q10, out int q11);
+            return Test(q00, q01, q10, q11);
 		}
 
+        /// <summary>
+        /// Log p-value calculation from q_xy of the contingency table.
+        /// </summary>
+        /// <param name="q00"></param>
+        /// <param name="q01"></param>
+        /// <param name="q10"></param>
+        /// <param name="q11"></param>
+        /// <returns></returns>
 		public static double GetLogFisherP(int q00, int q01, int q10, int q11){
 			int rowSum0 = q00 + q01;
 			int rowSum1 = q10 + q11;
@@ -27,26 +47,46 @@ namespace BaseLibS.Num.Test{
 					Factorial.LnValue(q11) - Factorial.LnValue(total);
 		}
 
-		public static void CalcContingency(bool[] x, bool[] y, out int r00, out int r01, out int r10, out int r11){
-			r00 = 0;
-			r01 = 0;
-			r10 = 0;
-			r11 = 0;
-			for (int i = 0; i < x.Length; i++){
-				if (x[i]){
-					if (y[i]){
-						r00++;
-					} else{
-						r01++;
-					}
-				} else{
-					if (y[i]){
-						r10++;
-					} else{
-						r11++;
-					}
-				}
+        /// <summary>
+        /// Calculate all 4 counts of the 2x2 contingency table.
+        /// Count for (x,y) in (xs, ys):
+        ///         |y=True |y=False|
+        /// |x=True |  r00  |  r10  |
+        /// |x=False|  r01  |  r11  |
+        /// </summary>
+        /// <param name="xs"></param>
+        /// <param name="ys"></param>
+        /// <param name="r00"></param>
+        /// <param name="r01"></param>
+        /// <param name="r10"></param>
+        /// <param name="r11"></param>
+		public static void CalcContingency(bool[] xs, bool[] ys, out int r00, out int r01, out int r10, out int r11){
+            var table = CalcContingencyTable(xs, ys);
+            r00 = table[0, 0];
+            r01 = table[0, 1];
+            r10 = table[1, 0];
+            r11 = table[1, 1];
+        }
+
+        /// <summary>
+        /// Calculate all 4 counts of the 2x2 contingency table.
+        /// Count for (x,y) in (xs, ys):
+        ///         |y=True |y=False|
+        /// |x=True |  r00  |  r10  |
+        /// |x=False|  r01  |  r11  |
+        /// </summary>
+        /// <param name="xs"></param>
+        /// <param name="ys"></param>
+        public static int[,] CalcContingencyTable(bool[] xs, bool[] ys)
+		{
+		    var table = new int[2, 2];
+			for (int i = 0; i < xs.Length; i++)
+			{
+			    int row = xs[i] ? 0 : 1;
+			    int col = ys[i] ? 0 : 1;
+			    table[row, col]++;
 			}
+		    return table;
 		}
 	}
 }
