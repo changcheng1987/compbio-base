b42e31ee820342fe02ef4625668c934769048774
rudolph <rudolph@biochem.mpg.de>
2018-02-07 13:27:52 +0100

    Add unit tests for un/pack array of *

---
+++ BaseLibS/BaseLibS.Test/Num/ArrayUtilsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BaseLibS.Num;
+using NUnit.Framework;
+
+namespace BaseLibS.Test.Num
+{
+    [TestFixture]
+    public class ArrayUtilsTest
+    {
+        [Test]
+        public void TestPackAndUnpackArrayOfArrays()
+        {
+            var arr = new[]
+            {
+                new[] {1, 2, 3, 4},
+                new[] {5, 6, 7, 8},
+            };
+            ArrayUtils.PackArrayOfArrays(arr, out var value, out var ind);
+            var arr2 = ArrayUtils.UnpackArrayOfArrays(value, ind);
+            Assert.AreEqual(arr.Length, arr2.Length);
+            CollectionAssert.AreEqual(arr[0], arr2[0]);
+            CollectionAssert.AreEqual(arr[1], arr2[1]);
+        }
+
+        [Test]
+        public void TestPackAndUnpackArrayOfStrings()
+        {
+            var arr = new[] { "1", "2", "3", "4"};
+            ArrayUtils.PackArrayOfStrings(arr, out var value, out var ind);
+            var arr2 = ArrayUtils.UnpackArrayOfStrings(value, ind);
+            CollectionAssert.AreEqual(arr, arr2);
+        }
+    }
+}
