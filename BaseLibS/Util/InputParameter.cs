54bb8838d8bb136878c57e471e5a9be43779bed7
Sinitsyn Pavel <pgsinitsyn@gmail.com>
2018-06-01 11:51:59 +0200

    FastaFileInfo => fastaFileInfo

--- BaseLibS/Util/InputParameter.cs
+++ BaseLibS/Util/InputParameter.cs
@@ -46,7 +46,7 @@ namespace BaseLibS.Util {
 					return "int";
 				}
 				if (t == typeof(FastaFileInfo)) {
-					return "FastaFileInfo";
+					return "fastaFileInfo";
 				}
 				throw new Exception("Unknown type: " + t);
 			}
